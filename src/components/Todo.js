52e6316cd2a13537cc477c40f7ae38c1fa017905
patricia <patriciaduarte@absistemas.com.br>
2022-06-24 08:53:49 -0300

    Criação dos componentes: Grupo de botões de filtro, checkbox e formulário.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,7 +5,7 @@ export default function Todo(props) {
         <li className="todo stack-small">
         <div className="c-cb">
           <input id="todo-0" type="checkbox" defaultChecked={props.completed} />
-          <label className="todo-label" htmlFor="">
+          <label className="todo-label" htmlFor="props.id">
             {props.name}
           </label>
         </div>
