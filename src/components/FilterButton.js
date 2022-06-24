720d5fad2d89f12c034ab959e153154cb2fd702f
patricia <patriciaduarte@absistemas.com.br>
2022-06-24 10:34:13 -0300

    Inserçã: Estado e useState Hook

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function FilterButton(props) {
   return (
-    <button type="button" className="btn toggle-btn" aria-pressed="true">
+    <button type="button"  onClick={() => alert("hi!")} className="btn toggle-btn" aria-pressed="true">
       <span className="visually-hidden">Show </span>
       <span>all </span>
       <span className="visually-hidden"> tasks</span>
